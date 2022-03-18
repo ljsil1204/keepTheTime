3546c5b046fc6e2509151c5889c9490760160316
이진실 <ljsil1204@naver.com>
2022-03-18 12:38:01 +0900

    #7 2.5초 뒤에 무조건 로그인화면으로 진입 - 임시

--- app/src/main/java/com/leejinsil/keepthetime/SplashActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/SplashActivity.kt
@@ -1,11 +1,32 @@
 package com.leejinsil.keepthetime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            val myIntent = Intent(mContext, SignInActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        }, 2500)
+
+    }
+
+    override fun setValues() {
     }
 }
