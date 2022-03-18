33231095393acb36e284718fce390eabf2b50f0e
이진실 <ljsil1204@naver.com>
2022-03-18 11:22:38 +0900

    #6 실제 서버 통신을 담당할 Retrofit 객체를 만들어주는 getRetrofit 함수 추가

--- app/src/main/java/com/leejinsil/keepthetime/api/ServerAPI.kt
+++ app/src/main/java/com/leejinsil/keepthetime/api/ServerAPI.kt
@@ -1,9 +1,30 @@
 package com.leejinsil.keepthetime.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ServerAPI {
 
-    private val BASE_URL = "https://keepthetime.xyz"
+    companion object{
 
-    
+        private val BASE_URL = "https://keepthetime.xyz"
+        private var retrofit : Retrofit? = null
+
+        fun getRetrofit () : Retrofit {
+
+            if (retrofit == null) {
+
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+            }
+
+            return retrofit!!
+
+        }
+
+    }
 
 }
