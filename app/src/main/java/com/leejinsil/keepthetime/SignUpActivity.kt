dc7b3db8ff7d7bc8de6ae7d0f109b1e3d6db4ddb
이진실 <ljsil1204@naver.com>
2022-03-18 13:01:04 +0900

    #9 회원가입 API 추가 / 호출 / 응답성공 시 토스트 띄우고 화면 종료

--- app/src/main/java/com/leejinsil/keepthetime/SignUpActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/SignUpActivity.kt
@@ -1,8 +1,13 @@
 package com.leejinsil.keepthetime
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivitySignUpBinding
+import com.leejinsil.keepthetime.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
 
@@ -23,7 +28,27 @@ class SignUpActivity : BaseActivity() {
             val inputPassword = binding.edtPassword.text.toString()
             val inputNickname = binding.edtNickname.text.toString()
 
+            apiList.putRequestSignUp(inputEmail, inputPassword, inputNickname).enqueue( object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    
+                    if (response.isSuccessful){
+                        
+                        val br = response.body()!!
+                        Toast.makeText(mContext, "회원가입에 성공하였습니다. ${br.data.user.nick_name}님, 가입을 축하합니다!", Toast.LENGTH_SHORT).show()
+                        finish()
+                        
+                    }
+                    
+                }
 
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                
+                }
+
+            })
 
         }
 
--- app/src/main/java/com/leejinsil/keepthetime/api/APIList.kt
+++ app/src/main/java/com/leejinsil/keepthetime/api/APIList.kt
@@ -5,6 +5,7 @@ import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface APIList {
 
@@ -13,6 +14,14 @@ interface APIList {
     fun postRequestLogin(
         @Field("email") email : String,
         @Field("password") password : String,
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("nick_name") nickname : String,
     ) : Call<BasicResponse>
 
 }
