700c38be0cc289a4ac6a45a6f65acda6a1e8daeb
이진실 <ljsil1204@naver.com>
2022-03-18 12:07:08 +0900

    #5 로그인 함수 호출 > 로그인 성공시 토스트 띄우기

--- app/src/main/java/com/leejinsil/keepthetime/SignInActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/SignInActivity.kt
@@ -1,8 +1,14 @@
 package com.leejinsil.keepthetime
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivitySignInBinding
+import com.leejinsil.keepthetime.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
     lateinit var binding : ActivitySignInBinding
@@ -22,7 +28,28 @@ class SignInActivity : BaseActivity() {
            val inputEmail = binding.edtEmail.text.toString()
            val inputPassword = binding.edtPassword.text.toString()
 
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue( object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
 
+                    if (response.isSuccessful){
+                        val br = response.body()!!
+                        Toast.makeText(mContext, "${br.data.user.nick_name}님, 로그인에 성공하셨습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        val br = response.errorBody()
+//                        Log.d("로그인 실패", br.toString())
+                    }
+                    
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            } )
 
         }
 
