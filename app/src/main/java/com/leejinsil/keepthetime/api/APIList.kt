c176542a8de1a393f8432335743ab17ea0715d5b
이진실 <ljsil1204@naver.com>
2022-03-18 11:51:12 +0900

    #5 APIList 인터페이스에 로그인 함수 명세 작성

--- app/src/main/java/com/leejinsil/keepthetime/api/APIList.kt
+++ app/src/main/java/com/leejinsil/keepthetime/api/APIList.kt
@@ -1,4 +1,18 @@
 package com.leejinsil.keepthetime.api
 
+import com.leejinsil.keepthetime.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface APIList {
+
+    @FormUrlEncoded
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email : String,
+        @Field("password") password : String,
+    ) : Call<BasicResponse>
+
 }
