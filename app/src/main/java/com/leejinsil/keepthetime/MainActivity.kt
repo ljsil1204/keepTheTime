897f0759c1970ad448aa865ee02c3e4998b57739
이진실 <ljsil1204@naver.com>
2022-03-21 12:15:49 +0900

    #14 bottomNav와 프래그먼트 페이지 연결

--- app/src/main/java/com/leejinsil/keepthetime/MainActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.leejinsil.keepthetime.adapters.MainViewPager2Adapter
 import com.leejinsil.keepthetime.databinding.ActivityMainBinding
 import com.leejinsil.keepthetime.utils.ContextUtil
@@ -21,6 +22,39 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        바텀 내비게이션
+        binding.mainBottomNav.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.navAppointment -> {
+                    binding.mainViewPager2.currentItem = 0
+                }
+                R.id.navFriend -> {
+                    binding.mainViewPager2.currentItem = 1
+                }
+                R.id.navProfile -> {
+                    binding.mainViewPager2.currentItem = 2
+                }
+            }
+
+            return@setOnItemSelectedListener true
+        }
+
+        binding.mainViewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                binding.mainBottomNav.selectedItemId = when(position) {
+                    0 -> R.id.navAppointment
+                    1 -> R.id.navFriend
+                    else -> R.id.navProfile
+                }
+
+            }
+
+        })
 
     }
 
