a465dbde942dc84c380b48cbc049baff3146c543
이진실 <ljsil1204@naver.com>
2022-03-21 16:54:58 +0900

    #15 서버에서주는 날짜 데이터 String -> Date 형태로 리턴하는 클래스 추가 > deserialize 함수 오버라이딩

---
+++ app/src/main/java/com/leejinsil/keepthetime/api/DateDeserializer.kt
@@ -0,0 +1,23 @@
+package com.leejinsil.keepthetime.api
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateDeserializer : JsonDeserializer<Date> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): Date {
+
+        val dateStr = json?.asString
+        val sdf = SimpleDateFormat("yyyy-MM-mm HH:mm:ss")
+        val date = sdf.parse(dateStr)
+
+        return date!!
+    }
+}
