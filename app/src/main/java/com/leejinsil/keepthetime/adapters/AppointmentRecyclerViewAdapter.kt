96a7c09c14064011ee99d400d69704ead9e12919
이진실 <ljsil1204@naver.com>
2022-03-21 13:17:40 +0900

    #15 리싸이클러뷰 어댑터 초기 작업

--- app/src/main/java/com/leejinsil/keepthetime/adapters/AppointmentRecyclerViewAdapter.kt
+++ app/src/main/java/com/leejinsil/keepthetime/adapters/AppointmentRecyclerViewAdapter.kt
@@ -1,4 +1,39 @@
 package com.leejinsil.keepthetime.adapters
 
-class AppointmentRecyclerViewAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.leejinsil.keepthetime.R
+import com.leejinsil.keepthetime.datas.AppointmentData
+
+class AppointmentRecyclerViewAdapter(
+    val mContext : Context,
+    val mList : List<AppointmentData>,
+) : RecyclerView.Adapter<AppointmentRecyclerViewAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view)  {
+
+        fun bind (data: AppointmentData) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item, parent, false)
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bind(data)
+
+    }
+
+    override fun getItemCount(): Int = mList.size
+
 }
