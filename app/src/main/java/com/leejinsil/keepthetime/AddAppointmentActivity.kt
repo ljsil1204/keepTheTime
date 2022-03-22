7c89decd51bfc507dfb6f3bba51155d602d121a5
이진실 <ljsil1204@naver.com>
2022-03-22 17:42:17 +0900

    #16 날짜 클릭시 날짜 선택창 나오도록

--- app/src/main/java/com/leejinsil/keepthetime/AddAppointmentActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/AddAppointmentActivity.kt
@@ -1,13 +1,21 @@
 package com.leejinsil.keepthetime
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityAddAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddAppointmentActivity : BaseActivity() {
 
     lateinit var binding : ActivityAddAppointmentBinding
+
+    val mSelectedAppointmentTime = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +26,40 @@ class AddAppointmentActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        날짜 선택 -> DatePickerDialog
+        binding.btnDay.setOnClickListener {
 
+            val dsl = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    mSelectedAppointmentTime.set(year, month, dayOfMonth)
+                    val sdf = SimpleDateFormat("yy/MM/dd (E)")
+                    binding.btnDay.text = sdf.format(mSelectedAppointmentTime.time)
+
+                }
+            }
+
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                mSelectedAppointmentTime.get(Calendar.YEAR),
+                mSelectedAppointmentTime.get(Calendar.MONTH),
+                mSelectedAppointmentTime.get(Calendar.DAY_OF_MONTH)
+            ).show()
+
+        }
+
+        binding.btnAppointmentSave.setOnClickListener {
+
+            val inputTitle = binding.edtTitle.text.toString()
+            val inputPlace = binding.edtPlace.text.toString()
+
+        }
 
     }
 
     override fun setValues() {
+
 
     }
 }
