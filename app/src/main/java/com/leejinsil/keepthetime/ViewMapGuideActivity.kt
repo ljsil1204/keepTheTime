43c9772a193f4785a62c25354fa2ed251bcce239
이진실 <ljsil1204@naver.com>
2022-03-23 18:11:56 +0900

    #20 ODsay 라이브러리 추가

--- app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityViewMapGuideBinding
 import com.leejinsil.keepthetime.datas.AppointmentData
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
 
 class ViewMapGuideActivity : BaseActivity() {
 
@@ -25,5 +28,27 @@ class ViewMapGuideActivity : BaseActivity() {
 
     override fun setValues() {
 
+        getNaverMapView()
+
     }
+
+    fun getNaverMapView () {
+
+        binding.naverMapView.getMapAsync {
+
+            val naverMap = it
+
+            val selectedLatLng = LatLng(mAppointmentData.latitude, mAppointmentData.longitude)
+
+            val cameraUpdate = CameraUpdate.scrollTo(selectedLatLng)
+            naverMap.moveCamera(cameraUpdate)
+
+            val marker = Marker()
+            marker.position = selectedLatLng
+            marker.map = naverMap
+
+        }
+
+    }
+
 }
