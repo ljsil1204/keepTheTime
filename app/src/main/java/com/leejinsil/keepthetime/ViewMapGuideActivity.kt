8bc701b366ceda48e0fd8f1470edf6a32e2e4a4b
이진실 <ljsil1204@naver.com>
2022-03-23 18:00:29 +0900

    #20 상세화면 데이터 받아오기

--- app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
@@ -3,14 +3,20 @@ package com.leejinsil.keepthetime
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityViewMapGuideBinding
+import com.leejinsil.keepthetime.datas.AppointmentData
 
 class ViewMapGuideActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewMapGuideBinding
 
+    lateinit var mAppointmentData : AppointmentData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_map_guide)
+        mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
