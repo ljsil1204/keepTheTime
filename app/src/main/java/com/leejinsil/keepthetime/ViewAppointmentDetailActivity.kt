41c0d8e4d8c41cba7370c51de07a0063ab2ce554
이진실 <ljsil1204@naver.com>
2022-03-23 17:07:34 +0900

    #17 첨부된 데이터 받기

--- app/src/main/java/com/leejinsil/keepthetime/ViewAppointmentDetailActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/ViewAppointmentDetailActivity.kt
@@ -3,14 +3,18 @@ package com.leejinsil.keepthetime
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityViewAppointmentDetailBinding
+import com.leejinsil.keepthetime.datas.AppointmentData
 
 class ViewAppointmentDetailActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewAppointmentDetailBinding
 
+    lateinit var mAppointmentData : AppointmentData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_appointment_detail)
+        mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
         setupEvents()
         setValues()
     }
