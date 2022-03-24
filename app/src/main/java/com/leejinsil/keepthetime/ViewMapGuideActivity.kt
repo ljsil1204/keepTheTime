ff00fb812e823b5f94088480106d01625d911254
이진실 <ljsil1204@naver.com>
2022-03-24 11:26:28 +0900

    #20 ODsay 라이브러리 api 호출 >  JSONObject를 로그로 확인

--- app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/ViewMapGuideActivity.kt
@@ -1,6 +1,7 @@
 package com.leejinsil.keepthetime
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityViewMapGuideBinding
 import com.leejinsil.keepthetime.datas.AppointmentData
@@ -8,6 +9,10 @@ import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PathOverlay
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 
 class ViewMapGuideActivity : BaseActivity() {
 
@@ -58,7 +63,54 @@ class ViewMapGuideActivity : BaseActivity() {
             path.coords = coordList
             path.map = naverMap
 
+            getPubTransPath()
+
         }
+
+    }
+
+    fun getPubTransPath() {
+
+        val ODsayService = ODsayService.init(mContext, "EyorgK6z5ndSx/83u/aKRDPZT3+TFWqHlUOZkc3JR6g")
+
+        val onResultCallbackListener = object : OnResultCallbackListener{
+
+            override fun onSuccess(odsayData: ODsayData?, api: API?) {
+
+                val jsonObj = odsayData!!.json!!
+                val resultObj = jsonObj.getJSONObject("result")
+
+                val busCount = resultObj.getInt("busCount")
+                val subwayCount = resultObj.getInt("subwayCount")
+                val subwayBusCount = resultObj.getInt("subwayBusCount")
+                val pointDistance = resultObj.getInt("pointDistance")
+
+                val pathArr = resultObj.getJSONArray("path")
+
+                for (i in 0 until pathArr.length()) {
+
+                    val pathObj = pathArr.getJSONObject(i)
+                    Log.d("pathObj", pathObj.toString())
+                }
+
+            }
+
+            override fun onError(p0: Int, p1: String?, p2: API?) {
+
+            }
+
+        }
+
+        ODsayService.requestSearchPubTransPath(
+            mAppointmentData.start_longitude.toString(),
+            mAppointmentData.start_latitude.toString(),
+            mAppointmentData.longitude.toString(),
+            mAppointmentData.latitude.toString(),
+            "0",
+            null,
+            null,
+            onResultCallbackListener
+        )
 
     }
 
