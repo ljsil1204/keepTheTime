65c266e9f64e23e1cf324b7fa12abf2af47e6b89
이진실 <ljsil1204@naver.com>
2022-03-25 10:21:52 +0900

    #20 교통목록 리스트 어탭터 추가/기초 작업

---
+++ app/src/main/java/com/leejinsil/keepthetime/adapters/PathListRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.leejinsil.keepthetime.adapters
+
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.leejinsil.keepthetime.R
+import com.leejinsil.keepthetime.datas.AppointmentData
+
+class PathListRecyclerViewAdapter(
+    val mContext : Context,
+    val mList : List<AppointmentData>,
+) : RecyclerView.Adapter<PathListRecyclerViewAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view)  {
+
+        fun bind (data: AppointmentData) {
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.path_list_item, parent, false)
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bind(data)
+
+    }
+
+    override fun getItemCount(): Int = mList.size
+
+}
