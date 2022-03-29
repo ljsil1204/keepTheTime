d75b8288776eb6368191985f1b5e234de37416a2
이진실 <ljsil1204@naver.com>
2022-03-29 21:44:58 +0900

    #21 사진 업로드 버튼 누를 시 갤러리 이동 > UI 반영

--- app/src/main/java/com/leejinsil/keepthetime/EditProfileActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/EditProfileActivity.kt
@@ -1,12 +1,22 @@
 package com.leejinsil.keepthetime
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.leejinsil.keepthetime.databinding.ActivityEditProfileBinding
+import com.leejinsil.keepthetime.utils.URIPathHelper
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 class EditProfileActivity : BaseActivity() {
 
     lateinit var binding : ActivityEditProfileBinding
+
+    val REQ_CODE_GALLERY = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +27,48 @@ class EditProfileActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnImageUpload.setOnClickListener {
+            getProfileImage()
+        }
+
     }
 
     override fun setValues() {
 
+
+
     }
+
+    fun getProfileImage() {
+
+        val myIntent = Intent()
+        myIntent.action = Intent.ACTION_PICK
+        myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult( myIntent, REQ_CODE_GALLERY )
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_GALLERY) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val selectedImageUri = data?.data!!
+
+                Glide.with(mContext).load(selectedImageUri).into(binding.imgProfile)
+
+//                val file = File(URIPathHelper().getPath(mContext, selectedImageUri))
+//
+//                val fileReqBody = RequestBody.create(MediaType.get("image/*"), file)
+//
+//                val mutiPartBody = MultipartBody.Part.createFormData("profile_image", "myProfile.jpg", fileReqBody)
+
+            }
+
+        }
+
+    }
+
 }
