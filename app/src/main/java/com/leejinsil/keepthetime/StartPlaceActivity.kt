25dbc09ac12e5bdef2ba5915a4269ac737301fce
이진실 <ljsil1204@naver.com>
2022-04-05 15:47:53 +0900

    #24 출발장소 Activity 추가   - 데이터 바인딩

---
+++ app/src/main/java/com/leejinsil/keepthetime/StartPlaceActivity.kt
@@ -0,0 +1,25 @@
+package com.leejinsil.keepthetime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.leejinsil.keepthetime.databinding.ActivityStartPlaceBinding
+
+class StartPlaceActivity : BaseActivity() {
+
+    lateinit var binding : ActivityStartPlaceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_start_place)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
