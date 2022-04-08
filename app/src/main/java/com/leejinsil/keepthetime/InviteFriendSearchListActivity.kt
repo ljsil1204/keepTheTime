55048ced051342bf86eb61fabe5d96783ade19fb
이진실 <ljsil1204@naver.com>
2022-04-08 18:04:15 +0900

    #31 내 친구 목록 화면에 보여주기   - 기존 내 친구목록 활용

--- app/src/main/java/com/leejinsil/keepthetime/InviteFriendSearchListActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/InviteFriendSearchListActivity.kt
@@ -2,11 +2,23 @@ package com.leejinsil.keepthetime
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.leejinsil.keepthetime.adapters.FriendListRecyclerAdapter
+import com.leejinsil.keepthetime.adapters.SearchedUserRecyclerAdapter
 import com.leejinsil.keepthetime.databinding.ActivityInviteFriendSearchListBinding
+import com.leejinsil.keepthetime.datas.BasicResponse
+import com.leejinsil.keepthetime.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class InviteFriendSearchListActivity : BaseActivity() {
 
     lateinit var binding : ActivityInviteFriendSearchListBinding
+
+    val mMyFriendsList = ArrayList<UserData>()
+
+    lateinit var mFriendAdapter : FriendListRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +29,49 @@ class InviteFriendSearchListActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+
+
     }
 
     override fun setValues() {
 
+        mFriendAdapter = FriendListRecyclerAdapter(mContext, mMyFriendsList)
+        binding.myFriendRecyclerView.adapter = mFriendAdapter
+        binding.myFriendRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
+
+
+    fun getMyFriendsFromServer(){
+
+        apiList.getrequestFriendList("my").enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    val br = response.body()!!
+
+                    mMyFriendsList.clear()
+
+                    mMyFriendsList.addAll(br.data.friends)
+
+                    mFriendAdapter.notifyDataSetChanged()
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getMyFriendsFromServer()
+    }
+
 }
