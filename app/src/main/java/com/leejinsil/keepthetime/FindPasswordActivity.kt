d900e276292201d151f0f9329a76d81caf5f158e
이진실 <ljsil1204@naver.com>
2022-04-11 14:26:40 +0900

    #29 api 호출 > 응답 성공/실패 시 토스트 띄우고 화면 종료

--- app/src/main/java/com/leejinsil/keepthetime/FindPasswordActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/FindPasswordActivity.kt
@@ -1,8 +1,14 @@
 package com.leejinsil.keepthetime
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.leejinsil.keepthetime.databinding.ActivityFindPasswordBinding
+import com.leejinsil.keepthetime.datas.BasicResponse
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FindPasswordActivity : BaseActivity() {
 
@@ -31,6 +37,35 @@ class FindPasswordActivity : BaseActivity() {
 
         val inputId = binding.edtId.text.toString()
         val inputNickname = binding.edtNickname.text.toString()
+
+        apiList.postRequestFindPassword(inputId, inputNickname).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    val br = response.body()!!
+
+                    Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
+                    finish()
+
+                }
+                else {
+
+                    val br = response.errorBody()!!.string()
+                    val jsonObj = JSONObject(br)
+                    val message = jsonObj.getString("message")
+
+                    Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                }
+
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
 
     }
 
