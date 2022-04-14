b823c3af7f19d1a6cc94bf2dddb409e3826b6680
이진실 <ljsil1204@naver.com>
2022-04-14 14:34:24 +0900

    #31 Activity popup 데이터 바인딩

--- app/src/main/java/com/leejinsil/keepthetime/ArrivalFriendActivity.kt
+++ app/src/main/java/com/leejinsil/keepthetime/ArrivalFriendActivity.kt
@@ -3,16 +3,17 @@ package com.leejinsil.keepthetime
 import android.app.Activity
 import android.os.Bundle
 import android.view.Window
-import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.leejinsil.keepthetime.databinding.ActivityArrivalFriendBinding
 
 class ArrivalFriendActivity : Activity() {
 
-//    lateinit var binding : ActivityArrivalFriendBinding
+    lateinit var binding : ActivityArrivalFriendBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_arrival_friend)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_arrival_friend)
         setContentView(R.layout.activity_arrival_friend)
     }
 
